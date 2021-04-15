6d871b252b2969ef228fa6ec10b37154a20da222
jaspermarsman <jaspermarsman@gmail.com>
2021-04-15 11:54:20 +0200

    functions-methods klaar en functions-scope eerste opdracht

--- functions-and-scope.js
+++ functions-and-scope.js
@@ -2,7 +2,7 @@
 // maar ook een manier moeten vinden om hetgeen dat je verzamelt ergens te bundelen. Op deze manier zul je ontdekken hoe je omgaat met scope. Pak vooral het hoofdstuk op EdHub over for-loops er nog eens bij!
 // Tip: je mag hier geen ingebouwde object methoden gebruiken, dus daar hoef je niet naar te kijken.
 
-const grades = [9, 8, 5, 7, 7, 4, 9, 8, 8, 3, 6, 8, 5, 6];
+
 
 /* Opdracht  1: Cum Laude */
 
@@ -15,6 +15,21 @@ const grades = [9, 8, 5, 7, 7, 4, 9, 8, 8, 3, 6, 8, 5, 6];
 // Log het antwoord in de terminal.
 
 // ---- Verwachte uitkomst: 6
+
+const grades = [9, 8, 5, 7, 7, 4, 9, 8, 8, 3, 6, 8, 5, 6];
+let amountCumlaude = 0;
+
+function cumLaudeCheck(){
+    for (let i=0; i < grades.length; i++){
+        if (grades[i] >= 8){
+            amountCumlaude = amountCumlaude +1;
+        }
+    }
+}
+
+cumLaudeCheck();
+console.log(amountCumlaude);
+
 
 
 /*  1b: Omschrijven tot een herbruikbare functie   */
